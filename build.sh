67c04a26dabe8617952d7eec4ee813105cd84454
FluffyKaon <github.fluffykaon@ss3t.net>
2013-10-12 17:04:20 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/java/jdk1.7.0_17/bin/javac -g -target 1.4 -source 1.4 -cp /usr/local/java_card_kit-2_2_2/lib/api.jar -d out/production/gpg_card/ java/net/ss3t/javacard/gpg/Gpg.java
---
+++ create_cap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp "/usr/local/java_card_kit-2_2_2/lib/*" com.sun.javacard.converter.Converter -config gpg.opt
